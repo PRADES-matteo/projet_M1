5191ef3fe96c49e26d8aebf2c9b5df7a2b200209
Lisa Blavot <bvtlisa@gmail.com>
2026-06-08 18:54:25 +0200

    redirection vers page login au lancement de l'app

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def home(request):
-    return render(request, "dashboard/home.html")
+    if request.user.is_authenticated:
+        return redirect("scenario-list")
+    return redirect("login")
